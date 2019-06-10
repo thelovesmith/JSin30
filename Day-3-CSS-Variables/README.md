99bf19d91293a99265d29ca5af3f9d56eef7df69
thelovesmith <averydante9@gmail.com>
2019-06-10 19:03:44 -0400

    finshed CSS var tutorial

--- Day-3-CSS-Variables/README.md
+++ Day-3-CSS-Variables/README.md
@@ -1,2 +1,6 @@
 # CSS VARIABLES and JavaScript
     * CSS Variables are new??Y You can update CSS variables with JS and when the variable is updated, it is rerendered everywhere it is referenced.
+
+    * document.QuerySelectorAll returns a NodeLost which is an array that comes within built methods, sometimes you need to change your nodelist into an array. 
+
+    *data attributes are a cool way to send data through your DOM Elements <div class='something' data-key='2b' data-{something} ="{the value}" >
--- Day-3-CSS-Variables/index.js
+++ Day-3-CSS-Variables/index.js
@@ -1,3 +1,14 @@
 //Select all inputs on the page with in the div with the class .controls and set them to the JS variable inputs 
 var inputs = document.querySelectorAll('.controls input');
 
+
+
+// function to be executed when somehting changes in the targeted object
+function handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix );
+    
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
