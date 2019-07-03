6b8804036450b2faf062851a0d136b4c92245f1d
thelovesmith <averydante9@gmail.com>
2019-07-03 09:42:42 -0400

    commiting last changes

--- Day-3-CSS-Variables/index.js
+++ Day-3-CSS-Variables/index.js
@@ -7,8 +7,11 @@ var inputs = document.querySelectorAll('.controls input');
 function handleUpdate() {
     const suffix = this.dataset.sizing || '';
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix );
+    console.log(document.documentElement.style, 'document element');
+    console.log('name', this.name);
+    console.log('value', this.value);
     
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+// inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
